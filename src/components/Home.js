dec680ec07653511a0668807bbdaf271e363abac
Divyansh Agarwal <a.divyansh.25@gmail.com>
2022-05-11 15:53:14 +0530

    Home component added

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navbar} from './Navbar/Navbar'
+import {Settings} from './Game settings/settings'
+
+export const Home = () => {
+  return (
+    <>
+        <Navbar />
+        <Settings />
+    </>
+  )
+}
